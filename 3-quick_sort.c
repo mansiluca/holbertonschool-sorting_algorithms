2690de3be4870f5cdd089e5b74c8d009f875569b
Luca Mansi <luca@mansi.fr>
2024-12-11 16:42:09 +0100

    Refactor quick_sort.c for improved readability and consistency

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-
 #include "sort.h"
 
 /**
  * swap - Swaps two elements in an array.
  * @a: Pointer to the first element.
  * @b: Pointer to the second element.
+ * Description: The values of the two integers are swapped.
  */
 void swap(int *a, int *b)
 {
-int temp = *a;
-*a = *b;
-*b = temp;
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
@@ -20,29 +22,33 @@ int temp = *a;
  * @low: The starting index of the partition.
  * @high: The ending index of the partition.
  * @size: The total size of the array.
- *
+ * Description: Uses the last element as the pivot.
  * Return: The index of the pivot.
  */
 int partition(int *array, int low, int high, size_t size)
 {
-int pivot = array[high];
-int i = low - 1;
+	int pivot;
+	int i;
 
-for (int j = low; j < high; j++)
-{
-	if (array[j] <= pivot)
+	i = low - 1;
+	pivot = array[high];
+
+
+	for (int j = low; j < high; j++)
 	{
-		i++;
-		swap(&array[i], &array[j]);
-		if (i != j)
-			print_array(array, size);
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+			if (i != j)
+				print_array(array, size);
+		}
 	}
-}
-swap(&array[i + 1], &array[high]);
-if (i + 1 != high)
-	print_array(array, size);
+	swap(&array[i + 1], &array[high]);
+	if (i + 1 != high)
+		print_array(array, size);
 
-return (i);
+	return (i);
 }
 
 /**
@@ -54,13 +60,13 @@ return (i);
  */
 void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
-if (low < high)
-{
-	int pivot = partition(array, low, high, size);
+	if (low < high)
+	{
+		int pivot = partition(array, low, high, size);
 
-	quick_sort_recursive(array, low, pivot - 1, size);
-	quick_sort_recursive(array, pivot + 1, high, size);
-}
+		quick_sort_recursive(array, low, pivot - 1, size);
+		quick_sort_recursive(array, pivot + 1, high, size);
+	}
 }
 
 /**
@@ -70,8 +76,8 @@ if (low < high)
  */
 void quick_sort(int *array, size_t size)
 {
-if (array == NULL || size < 2)
-	return;
+	if (array == NULL || size < 2)
+		return;
 
-quick_sort_recursive(array, 0, size - 1, size);
+	quick_sort_recursive(array, 0, size - 1, size);
 }
